8d22684e57940da2f7bcedfd490171db02e6280e
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-06 18:40:56 +0530

    feat : adding merge sort

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -1,6 +1,51 @@
 import React, {Component} from 'react';
 import './Visualizer.css';
 
+// generating random number 
+function randomNumInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+// merge sort algorithm 
+function helpMergeSort(array){
+    console.log("before: "+array)
+    if(array.length<=1)return array
+    const auArray = array.slice()
+    MergeSort(array,0,array.length-1,auArray)
+    console.log("after: "+array)
+}
+
+function merge(array,l,mid,h,b){
+    let i = l
+    let j = mid + 1
+    let k = l
+    while(i<=mid && j<=h){
+        if(b[i]<b
+            [j]){
+            array[k++]=b[i++]
+        }
+        else{
+            array[k++] = b[j++]
+        }
+    }
+    while(i<=mid){
+        array[k++] = b[i++]
+    }
+    while(j<=h){
+        array[k++] = b[j++]
+    }
+}
+
+function MergeSort(array,l,h,auArray){
+    if (l===h)return;
+    // console.log("before sorting "+array)
+    let mid=Math.floor((l+h)/2)
+    MergeSort(auArray,l,mid,array)
+    MergeSort(auArray,mid+1,h,array)
+    merge(array,l,mid,h,auArray)
+    
+}
+
 export default class Visualizer extends Component{
     constructor(props){
         super(props)
@@ -21,25 +66,31 @@ export default class Visualizer extends Component{
         this.setState({array})
         // console.log(this.setState(array))
     }
+
+    // MergeSort(array,l,h){
+        
+    //     this.setState({array})
+    // }
     
     render(){
         const {array} = this.state
-        console.log(array)
+        // console.log(array)
         return (
-            <div className="container">
-                {array.map((value,id)=>(
-                <div className="container-box">
-                    <div className="num">{value}</div>
-                    <div className="bar" key={id} style={{height:`${value*2}px`}}></div>
-                    
-                </div>))}
-            </div>
+            <>
+                <div className="container">
+                    {array.map((value,id)=>(
+                    <div className="container-box">
+                        <div className="num">{value}</div>
+                        <div className="bar" key={id} style={{height:`${value*2}px`}}></div>
+                        
+                    </div>))}
+                </div>
+                <button onClick={()=>(this.resetArray())}>reset</button>
+                <button onClick={()=>(helpMergeSort(array))}>Merge Sort</button>
+            </>
         )
     }
     
 }
 
-function randomNumInterval(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min)
-}
-// export default Visualizer;
+
