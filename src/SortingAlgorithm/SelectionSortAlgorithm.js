f6f66195afd6e8c4fc19bf9257884dd112bc0c8f
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-10 17:10:42 +0530

    feat : adding selection sort algorithm

--- src/SortingAlgorithm/SelectionSortAlgorithm.js
+++ src/SortingAlgorithm/SelectionSortAlgorithm.js
@@ -1,3 +1,23 @@
-export function SSort(array){
+function SelectionSort(array){
+    console.log(array)
     
+    let i,j,k,temp
+    for (i=0;i<array.length-1;i++){
+        for(j=k=i;j<array.length;j++){
+            if(array[j]<array[k]){
+                k=j
+            }
+        }
+        temp = array[i]
+        array[i] = array[k]
+        array[k] = temp
+    }
+    console.log(array)
+}
+
+export function SSort(array){
+    if (array.length<=1){
+        return array
+    }
+    SelectionSort(array)
 }
