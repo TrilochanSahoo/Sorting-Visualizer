4c2e9eb7ca50add71d14008c0531e933b20717e6
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-10 15:44:29 +0530

    feat : adding animation to insertion sort

--- src/SortingAlgorithm/InsertionSortAlgorithm.js
+++ src/SortingAlgorithm/InsertionSortAlgorithm.js
@@ -1,21 +1,40 @@
-function InsertionSort(array){
+function InsertionSort(array,animations){
     let x,j
     for(let i=1;i<array.length;i++){
         j=i-1
         x=array[i]
         while(j>-1 && array[j]>x){
+            const animation = {}
+            animation.comparision=[j+1,j]
+            animation.swap1=[j+1,array[j]]
+            animation.swap2=[j,array[j+1]]
             array[j+1]=array[j]
             j--
+            animations.push(animation)
+        }
+        if(j===-1){
+            
+        }
+        else{
+            const animation = {}
+            animation.comparision=[j,j+1]
+            animation.swap1=[j+1,x]
+            animation.swap2=[j,array[j]]
+            animations.push(animation)
         }
         array[j+1]=x
+        // animation.swap=[j+1,array[i]]
     }
     console.log(array)
 }
 
 
 export function ISort(array){
+    const animations = []
+    console.log(array)
     if(array.length<=1){
         return array
     }
-    InsertionSort(array)
+    InsertionSort(array,animations)
+    return animations
 }
