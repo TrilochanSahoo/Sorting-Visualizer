83cd599e212ed5aa61a426fbfc65fba50ba94b5e
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-11 15:59:54 +0530

    update : adding animation to Selection sort

--- src/SortingAlgorithm/SelectionSortAlgorithm.js
+++ src/SortingAlgorithm/SelectionSortAlgorithm.js
@@ -1,23 +1,40 @@
-function SelectionSort(array){
+function SelectionSort(array,animations){
     console.log(array)
     
     let i,j,k,temp
     for (i=0;i<array.length-1;i++){
         for(j=k=i;j<array.length;j++){
+            const animation={}
             if(array[j]<array[k]){
                 k=j
+                animation.comparision =[k,j]
+                animation.swap1=[j,array[j]]
+                animation.swap2=[k,array[k]]
+            }else{
+                animation.comparision =[k,j]
+                animation.swap1=[j,array[j]]
+                animation.swap2=[k,array[k]]
             }
+            animations.push(animation)
         }
+        const animation = {}
+        animation.comparision =[i,k]
+        animation.swap1=[i,array[k]]
+        animation.swap2=[k,array[i]]
+
         temp = array[i]
         array[i] = array[k]
         array[k] = temp
+        animations.push(animation)
     }
     console.log(array)
 }
 
 export function SSort(array){
+    const animations = []
     if (array.length<=1){
         return array
     }
-    SelectionSort(array)
+    SelectionSort(array,animations)
+    return animations
 }
--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -166,6 +166,49 @@ export default class Visualizer extends Component{
     
     SelectionSort(){
         const animations = SSort(this.state.array)
+
+        const newAnimations = []
+        for(const animation of animations){
+            newAnimations.push(animation.comparision)
+            newAnimations.push(animation.comparision)
+            newAnimations.push(animation.swap1)
+            newAnimations.push(animation.swap2)
+        }
+        console.log(newAnimations)
+        for(let i=0; i<newAnimations.length;i++){
+            const arrayBars = document.getElementsByClassName("bar");
+            if ((i % 4) !==2 && (i % 4) !==3){
+                const [barOneIdx, barTwoIdx] = newAnimations[i]
+                const barOneStyle = arrayBars[barOneIdx].style
+                const barTwoStyle = arrayBars[barTwoIdx].style
+
+                const color= i % 4 ===0?'red':'turquoise';
+                setTimeout(()=>{
+                    barOneStyle.backgroundColor = color
+                    barTwoStyle.backgroundColor = color
+                },i*20)
+                
+            }
+            else{
+                if((i % 4) ===2 ){
+                    setTimeout(()=>{
+                        const [barOneIdx, newHeight] = newAnimations[i]
+                        const barOneValue = arrayBars[barOneIdx]
+                        const barOneStyle = arrayBars[barOneIdx].style
+                        barOneStyle.height = `${newHeight*2}px`
+                        barOneValue.innerHTML = `${newHeight}`
+                    },i*20)
+                }else{
+                    setTimeout(()=>{
+                        const [barTwoIdx, newHeight] = newAnimations[i]
+                        const barTwoValue = arrayBars[barTwoIdx]
+                        const barTwoStyle = arrayBars[barTwoIdx].style
+                        barTwoStyle.height = `${newHeight*2}px`
+                        barTwoValue.innerHTML = `${newHeight}`
+                    },i*20)
+                }
+            }
+        }
     }
 
     render(){
