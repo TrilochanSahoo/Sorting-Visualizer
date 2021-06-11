f23f3492b34358525e45c4dd2e115f9c73b869b2
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-11 18:41:25 +0530

    feat : adding animation to quick sort

--- src/SortingAlgorithm/QuickSortAlgorithm.js
+++ src/SortingAlgorithm/QuickSortAlgorithm.js
@@ -1,15 +1,29 @@
-function partition(array,l,h){
+function partition(array,l,h,animations){
     let temp
     let pivot =array[h]
     let i=l-1
     for(let j= l;j<h;j++){
+        const animation = {}
+        animation.comparision =[h,j]
         if(array[j]<=pivot){
             i++
             temp = array[i]
             array[i] = array[j]
             array[j] = temp
+            animation.swap1=[i,array[i]]
+            animation.swap2=[j,array[j]]
+        }else{
+            animation.swap1=[h,pivot]
+            animation.swap2=[j,array[j]]
+
         }
+        animations.push(animation)
     }
+    const animation = {}
+    animation.comparision = [i+1,h]
+    animation.swap1 = [i+1,array[h]]
+    animation.swap2 = [h,array[i+1]]
+    animations.push(animation)
 
     temp = array[i+1]
     array[i+1] = array[h]
@@ -18,24 +32,27 @@ function partition(array,l,h){
     return i+1
 }
 
-function QuickSort(array,l,h){
+function QuickSort(array,l,h,animations){
     // console.log(array)
     let j
     if(l<h){
-        j =partition(array,l,h)
-        QuickSort(array,l,j-1)
-        QuickSort(array,j+1,h)
+        j =partition(array,l,h,animations)
+        console.log(j)
+        QuickSort(array,l,j-1,animations)
+        QuickSort(array,j+1,h,animations)
     }
     // console.log(array)
 }
 
 
 export function QSort(array){
+    const animations = []
     console.log(array)
     if (array.length<=1){
         return array
     }
-    QuickSort(array,0,array.length-1)
+    QuickSort(array,0,array.length-1,animations)
     console.log(array)
+    return animations
 
 }
--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -140,7 +140,7 @@ export default class Visualizer extends Component{
                 setTimeout(()=>{
                     barOneStyle.backgroundColor = color
                     barTwoStyle.backgroundColor = color
-                },i*5)
+                },i*20)
                 
             }
             else{
@@ -151,7 +151,7 @@ export default class Visualizer extends Component{
                         const barOneStyle = arrayBars[barOneIdx].style
                         barOneStyle.height = `${newHeight*2}px`
                         barOneValue.innerHTML = `${newHeight}`
-                    },i*5)
+                    },i*20)
                 }else{
                     setTimeout(()=>{
                         const [barTwoIdx, newHeight] = newAnimations[i]
@@ -159,7 +159,7 @@ export default class Visualizer extends Component{
                         const barTwoStyle = arrayBars[barTwoIdx].style
                         barTwoStyle.height = `${newHeight*2}px`
                         barTwoValue.innerHTML = `${newHeight}`
-                    },i*5)
+                    },i*20)
                 }
             }
         }
@@ -214,6 +214,49 @@ export default class Visualizer extends Component{
 
     QuickSort(){
         const animations = QSort(this.state.array)
+
+        const newAnimations = []
+        for(const animation of animations){
+            newAnimations.push(animation.comparision)
+            newAnimations.push(animation.comparision)
+            newAnimations.push(animation.swap1)
+            newAnimations.push(animation.swap2)
+        }
+        console.log(newAnimations)
+        for(let i=0; i<newAnimations.length;i++){
+            const arrayBars = document.getElementsByClassName("bar");
+            if ((i % 4) !==2 && (i % 4) !==3){
+                const [barOneIdx, barTwoIdx] = newAnimations[i]
+                const barOneStyle = arrayBars[barOneIdx].style
+                const barTwoStyle = arrayBars[barTwoIdx].style
+
+                const color= i % 4 ===0?'red':'turquoise';
+                setTimeout(()=>{
+                    barOneStyle.backgroundColor = color
+                    barTwoStyle.backgroundColor = color
+                },i*20)
+                
+            }
+            else{
+                if((i % 4) ===2 ){
+                    setTimeout(()=>{
+                        const [barOneIdx, newHeight] = newAnimations[i]
+                        const barOneValue = arrayBars[barOneIdx]
+                        const barOneStyle = arrayBars[barOneIdx].style
+                        barOneStyle.height = `${newHeight*2}px`
+                        barOneValue.innerHTML = `${newHeight}`
+                    },i*20)
+                }else{
+                    setTimeout(()=>{
+                        const [barTwoIdx, newHeight] = newAnimations[i]
+                        const barTwoValue = arrayBars[barTwoIdx]
+                        const barTwoStyle = arrayBars[barTwoIdx].style
+                        barTwoStyle.height = `${newHeight*2}px`
+                        barTwoValue.innerHTML = `${newHeight}`
+                    },i*20)
+                }
+            }
+        }
     }
 
     render(){
