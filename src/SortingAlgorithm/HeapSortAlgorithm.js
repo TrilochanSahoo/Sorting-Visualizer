860670bff2f2113b5750a6d8b43568008fe8ce12
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-16 18:04:50 +0530

    feat: adding animation to hep sort

--- src/SortingAlgorithm/HeapSortAlgorithm.js
+++ src/SortingAlgorithm/HeapSortAlgorithm.js
@@ -1,17 +1,32 @@
 let length
-function maxHeap(array,i){
+function maxHeap(array,i,animations){
     const left = 2*i+1
     const right = 2*i+2
     let max = i
     if(left<length && array[left]>array[max]){
+        const animation ={}
+        animation.comparision = [max,left]
+        animation.swap1 = [max,array[left]]
+        animation.swap2 = [left,array[max]]
         max=left
+        animations.push(animation)
     }
     if(right<length && array[right]>array[max]){
+        const animation ={}
+        animation.comparision = [max,right]
+        animation.swap1 = [max,array[right]]
+        animation.swap2 = [right,array[max]]
         max=right
+        animations.push(animation)
     }
-    if(max != i){
+    if(max !== i){
+        const animation ={}
+        animation.comparision = [i,max]
+        animation.swap1 = [i,array[max]]
+        animation.swap2 = [max,array[i]]
+        animations.push(animation)
         swap(array,i,max)
-        maxHeap(array,max)
+        maxHeap(array,max,animations)
     }
 }
 
@@ -22,18 +37,25 @@ function swap(array,x,y){
 }
 
 export function HSort(array){
+    const animations = []
     length = array.length
     for(let i=Math.floor(length/2);i>=0;i--){
-        maxHeap(array,i)
+        maxHeap(array,i,animations)
     }
 
     console.log(array)
     for(let i=array.length-1;i>0;i--){
+        const animation ={}
+        animation.comparision = [0,i]
+        animation.swap1 = [0,array[i]]
+        animation.swap2 = [i,array[0]]
+        animations.push(animation)
         swap(array,0,i)
         length--
-        maxHeap(array,0)
+        maxHeap(array,0,animations)
     }
 
     console.log(array)
+    return animations
 
 }
